b6a5eda75310c85cb6eaee4664b7a5189775e9f1
gesse_carlos <gesse.carlos@outlook.com>
2023-01-29 18:17:51 -0300

    feat: mysql db config file for testing only

--- customer_manager/settings.py
+++ customer_manager/settings.py
@@ -75,10 +75,13 @@ WSGI_APPLICATION = 'customer_manager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/path/to/my.cnf',
+        },
     }
 }
+
 
 
 # Password validation
