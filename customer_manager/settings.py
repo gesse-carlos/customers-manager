0f2af37f2efdccc9f8fae3297545f16af59df6a9
gesse_carlos <gesse.carlos@outlook.com>
2023-01-29 18:29:08 -0300

    feat: cep middleware

--- customer_manager/settings.py
+++ customer_manager/settings.py
@@ -50,6 +50,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    # cep validation
+    'customers.middleware.CepValidate',
 ]
 
 ROOT_URLCONF = 'customer_manager.urls'
